71bedd1703445ea2139ea7214ab057f22b7baeca
Sergey <xxsmotur@gmail.com>
2020-08-02 13:30:30 +0500

    implementation of filter transform

---
+++ examples/filters/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	rec "github.com/saromanov/voodoo/pkg/receiver/channel"
+	sChannel "github.com/saromanov/voodoo/pkg/source/channel"
+	"github.com/saromanov/voodoo/pkg/source/redis"
+	"github.com/saromanov/voodoo/pkg/transform/filter"
+	"github.com/saromanov/voodoo/pkg/voodoo"
+)
+
+func filterTransform(data interface{}) bool {
+	r := data.(string)
+	fmt.Println(r)
+	return len(r) > 5
+}
+
+func sourceChannel() <-chan interface{} {
+	result := make(chan interface{})
+	time.Sleep(2 * time.Second)
+	go func() {
+		for i := 0; i < 25; i++ {
+			result <- fmt.Sprintf("%v", i)
+		}
+	}()
+	return result
+}
+func reca(data interface{}) {
+	fmt.Println(data)
+}
+
+func main() {
+	v := voodoo.New()
+	source, err := redis.New(context.TODO(), &redis.Options{Channel: "test"})
+	if err != nil {
+		panic(err)
+	}
+
+	source2, err := redis.New(context.TODO(), &redis.Options{Channel: "test2"})
+	if err != nil {
+		panic(err)
+	}
+
+	source3, err := sChannel.New(context.TODO(), &sChannel.Options{
+		Method: sourceChannel,
+	})
+
+	receiver, err := rec.New(context.Background(), reca)
+	if err != nil {
+		panic(err)
+	}
+	v.AddSources(source).
+		AddSources(source2).
+		AddSources(source3).
+		Transform(filter.New(filterTransform)).
+		AddReceivers(receiver).Do()
+	select {}
+}
---
+++ pkg/transform/filter/filter.go
@@ -0,0 +1,49 @@
+package filter
+
+import (
+	"github.com/saromanov/voodoo/pkg/transform"
+)
+
+// Func defines function for transformation
+type Func func(interface{}) bool
+
+type Filter struct {
+	F   Func
+	in  chan interface{}
+	out chan interface{}
+}
+
+// New initialize method for map
+func New(f Func) transform.Transform {
+	r := &Filter{
+		F:   f,
+		in:  make(chan interface{}),
+		out: make(chan interface{}),
+	}
+	go r.apply()
+	return r
+}
+
+// In receive data for processing
+func (m *Filter) In(elem interface{}) {
+	m.in <- elem
+	return
+}
+
+// Out returns processed data
+func (m *Filter) Out() <-chan interface{} {
+	return m.out
+}
+
+// apply provides doing of map data
+func (m *Filter) apply() {
+	for elem := range m.in {
+		go func(e interface{}) {
+			res := m.F(e)
+			if res {
+				m.out <- res
+			}
+		}(elem)
+	}
+	close(m.out)
+}
