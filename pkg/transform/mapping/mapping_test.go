8f48c2652baeb37d5811831fb5a2170eeda97280
Sergey <xxsmotur@gmail.com>
2020-08-15 23:02:18 +0500

    Add test for mapping

---
+++ pkg/transform/mapping/mapping_test.go
@@ -0,0 +1,30 @@
+package mapping
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func mapping(d interface{}) interface{} {
+	return strings.ToUpper(d.(string))
+}
+func TestChannel(t *testing.T) {
+
+	ctx, cancel := context.WithTimeout(context.TODO(), 2*time.Second)
+	defer cancel()
+	r := New(mapping)
+	go func() {
+		r.In("msg")
+	}()
+
+	select {
+	case <-ctx.Done():
+		t.Fail()
+	case res := <-r.Out():
+		assert.Equal(t, res, "MSG")
+	}
+}
