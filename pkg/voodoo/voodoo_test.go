d1c3952703821fb88a0faab4345aa0bc79680fca
Sergey <xxsmotur@gmail.com>
2020-08-17 22:02:41 +0500

    Draft test for test register

--- pkg/voodoo/voodoo_test.go
+++ pkg/voodoo/voodoo_test.go
@@ -3,10 +3,13 @@ package voodoo
 import (
 	"context"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
+	rec "github.com/saromanov/voodoo/pkg/receiver/channel"
 	sChannel "github.com/saromanov/voodoo/pkg/source/channel"
+	"github.com/saromanov/voodoo/pkg/transform/mapping"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,6 +24,10 @@ func sourceChannel() <-chan interface{} {
 	return result
 }
 
+func mapTransform(data interface{}) interface{} {
+	return strings.ToUpper(data.(string))
+}
+
 func TestAddSource(t *testing.T) {
 	n := New()
 	assert.Error(t, errNoSources, n.AddSources().Do())
@@ -31,4 +38,19 @@ func TestAddSource(t *testing.T) {
 	assert.NoError(t, err)
 	n = New()
 	assert.Error(t, errNoReceivers, n.AddSources(source).Do())
+}
+
+func TestRegister(t *testing.T) {
+	n := New()
+	source, err := sChannel.New(context.TODO(), &sChannel.Options{
+		Method: sourceChannel,
+	})
+
+	reca := func(data interface{}) {
+
+	}
+	receiver, err := rec.New(context.Background(), reca)
+	assert.NoError(t, err)
+	assert.NoError(t, err)
+	n.AddSources(source).Transform(mapping.New(mapTransform)).AddReceivers(receiver)
 }
